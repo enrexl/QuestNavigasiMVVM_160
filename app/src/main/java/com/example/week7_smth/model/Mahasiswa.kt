0ee50b83f29f4ab114d9349a14b2fc7f4865402e
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-11-17 10:16:06 +0700

    Menambah variable 'email,noHp' dan 'nim' ke dalam FormulirView.kt

--- app/src/main/java/com/example/week7_smth/model/Mahasiswa.kt
+++ app/src/main/java/com/example/week7_smth/model/Mahasiswa.kt
@@ -3,5 +3,8 @@ package com.example.week7_smth.model
 data class Mahasiswa(
     val nama :String="",
     val gender :String="",
-    val alamat : String=""
+    val alamat : String="",
+    val email : String="",
+    val noHp : String="",
+    val nim: String="",
 )
